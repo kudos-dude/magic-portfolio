854f53ce523ade358a49fbd36a5f9371e59763a6
Weston Wieser <deiasr@gmail.com>
2019-06-25 22:47:53 -0400

    regeneratorRuntime

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,34 +1,34 @@
-import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import path from "path";
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
 
-import resolve from "./webpack.config.resolve";
+import resolve from './webpack.config.resolve';
 
 const GLOBALS = {
-  "process.env.NODE_ENV": JSON.stringify("production"),
-  __DEV__: false
+  'process.env.NODE_ENV': JSON.stringify('production'),
+  __DEV__: false,
 };
 
 export default {
   resolve,
-  devtool: "source-map",
-  entry: path.resolve(__dirname, "src/index"),
-  target: "web",
-  mode: "production",
+  devtool: 'source-map',
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src/index')],
+  target: 'web',
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-    filename: "[name].[contenthash].js"
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[contenthash].js',
   },
   plugins: [
     new webpack.DefinePlugin(GLOBALS),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css"
+      filename: '[name].[contenthash].css',
     }),
     new HtmlWebpackPlugin({
-      template: "src/index.ejs",
-      favicon: "src/favicon.ico",
+      template: 'src/index.ejs',
+      favicon: 'src/favicon.ico',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -39,107 +39,107 @@ export default {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true
+        minifyURLs: true,
       },
       inject: true,
-      trackJSToken: ""
-    })
+      trackJSToken: '',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.eot(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
-              name: "[name].[ext]"
-            }
-          }
-        ]
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 10000,
-              mimetype: "application/font-woff",
-              name: "[name].[ext]"
-            }
-          }
-        ]
+              mimetype: 'application/font-woff',
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 10000,
-              mimetype: "application/octet-stream",
-              name: "[name].[ext]"
-            }
-          }
-        ]
+              mimetype: 'application/octet-stream',
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 10000,
-              mimetype: "image/svg+xml",
-              name: "[name].[ext]"
-            }
-          }
-        ]
+              mimetype: 'image/svg+xml',
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.(jpe?g|png|gif|ico)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]"
-            }
-          }
-        ]
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /(\.css|\.scss|\.sass)$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              sourceMap: true
-            }
+              sourceMap: true,
+            },
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               // eslint-disable-next-line
-              plugins: () => [require("cssnano"), require("autoprefixer")],
-              sourceMap: true
-            }
+              plugins: () => [require('cssnano'), require('autoprefixer')],
+              sourceMap: true,
+            },
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
-              includePaths: [path.resolve(__dirname, "src")],
-              sourceMap: true
-            }
-          }
-        ]
-      }
-    ]
-  }
+              includePaths: [path.resolve(__dirname, 'src')],
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
